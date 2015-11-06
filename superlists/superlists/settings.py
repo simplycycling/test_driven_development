3659cea6550a28cd1e3aee326b67735b6bc878d4
Roger Sherman <rsherman@viddler.com>
2015-11-06 01:48:54 -0500

    Refactor home page view to use as template.

--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lists',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'superlists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'lists/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
